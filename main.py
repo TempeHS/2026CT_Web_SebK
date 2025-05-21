8e00ed94fab1ec58173c8dd0cb00afe8955a4b0d
Sebastian K <sebastian.king19@education.nsw.gov.au>
2025-05-21 01:54:53 +0000

    morecard

--- main.py
+++ main.py
@@ -6,7 +6,13 @@ app = Flask(__name__)
 @app.route('/index.html')
 @app.route('/')
 def index():
-    return render_template("index.html"), 200
+    card_data = (
+        ("Golden Cookie Combos", "How to use golden cookies together", "Find Out Here!", "static/images/cardimg1.png"),
+        ("Best Krumblor Abilites", "How to train your cookie dragon", "Find Out Here!", "static/images/cardimg2.png"),
+        ("Minigame Guide", "How to maximise your minigame game", "Find Out Here!", "static/images/cardimg3.png"),
+    )
+
+    return render_template("index.html", cards=card_data), 200
 
 if __name__ == '__main__':
     app.run(debug=True)
