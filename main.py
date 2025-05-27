27fcadc2e7baa3787831f534b897289f5d45eb4a
Sebastian K <sebastian.king19@education.nsw.gov.au>
2025-05-27 23:39:29 +0000

    cardformat

--- main.py
+++ main.py
@@ -7,9 +7,12 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     card_data = (
-        ("Golden Cookie Combos", "How to use golden cookies together", "Find Out Here!", "static/images/cardimg1.png"),
-        ("Best Krumblor Abilites", "How to train your cookie dragon", "Find Out Here!", "static/images/cardimg2.png"),
-        ("Minigame Guide", "How to maximise your minigame game", "Find Out Here!", "static/images/cardimg3.png"),
+        ("Golden Cookie Combos", "How to use golden cookies together", "webpage", "static/images/cardimg1.png"),
+        ("Best Krumblor Abilites", "How to train your cookie dragon", "webpage", "static/images/cardimg2.png"),
+        ("Minigame Guide", "How to maximise your minigame game", "webpage", "static/images/cardimg3.png"),
+        ("Minigame Guide2", "How to maximise your minigame game", "webpage", "static/images/cardimg3.png"),
+        ("Minigame Guide3", "How to maximise your minigame game", "webpage", "static/images/cardimg3.png"),
+        ("Minigame Guide4", "How to maximise your minigame game", "webpage", "static/images/cardimg3.png"),
     )
 
     return render_template("index.html", cards=card_data), 200
